ff5055db4c831c1a1c588c34d9c98e31f8a1df88
ASAngelica <asa196549@gmail.com>
2022-02-22 17:36:09 -0300

    Insert seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,39 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
 		PreparedStatement st = null;
 		try {
 			conn = DB.getConnection();
 			
 			st = conn.prepareStatement(
-					"insert into department (Name) values ('D1'), ('D2')",
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)",
 					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, "Carl Purple");
+			st.setString(2, "carl@gmail.com");
+			st.setDate(3, new Date(sdf.parse("22/04/1985").getTime()));
+			st.setDouble(4, 3000.00);
+			st.setInt(5, 4);
+
 			
 			int rowsAffected = st.executeUpdate();
 			
@@ -32,6 +47,9 @@ public class Program {
 			
 		}
 		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		catch(ParseException e) {
 			e.printStackTrace();
 		}
 		finally {
